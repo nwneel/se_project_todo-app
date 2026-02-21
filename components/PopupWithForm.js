b8d1eceed2683a20c85a7d1a99390c762758bded
Nathan Neel <nwneel@hotmail.com>
2026-02-20 21:04:50 -0800

    New update for my project

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -26,6 +26,7 @@ class PopupWithForm extends Popup {
     this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
+      this.close();
     });
   }
 }
